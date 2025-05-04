ce4deaf4e7934f61ce3d107d9cad16286cce7522
Beeeeho <ross6857@gmail.com>
2025-05-05 00:47:57 +0900

    Player Control, Gameplay UI Updated

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+using TMPro;
+using StarterAssets;
+
+public class GameManager : MonoBehaviour
+{
+    public TMP_Text timerText;
+    public Image skill1Icon;
+    public TMP_Text skill1Description;
+    public GameObject pauseMenuUI;
+
+    private bool isPaused = false;
+    public bool isSkill1Available = true;
+
+    private float timeRemaining = 120.0f;
+
+    [SerializeField] private GameObject player;
+    private StarterAssetsInputs playerInput;
+
+    private void Start()
+    {
+        playerInput = player.GetComponent<StarterAssetsInputs>();
+    }
+
+    void Update()
+    {
+        UpdateTimerUI();
+        UpdateSkillUI();
+
+        /* if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused) {ResumeGame();}
+            else {PauseGame();}
+        } */
+
+        if (Keyboard.current.escapeKey.wasPressedThisFrame)
+    {
+        if (isPaused)
+            ResumeGame();
+        else
+            PauseGame();
+    }
+
+    }
+
+    private void UpdateTimerUI()
+    {
+        if (timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+            int minutes = Mathf.FloorToInt(timeRemaining / 60);
+            int seconds = Mathf.FloorToInt(timeRemaining % 60);
+            timerText.text = string.Format("Timer: {0:00}:{1:00}", minutes, seconds);
+        }
+        else
+        {
+            timerText.text = "00:00";
+        }
+    }
+
+    private void UpdateSkillUI()
+    {
+        skill1Icon.color = isSkill1Available ? Color.white : Color.gray;
+
+        skill1Description.text = "[Skill] Press E, Allows to walk on water";
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+        pauseMenuUI.SetActive(true);
+        isPaused = true;
+
+        if (playerInput != null)
+            playerInput.enabled = false;
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        pauseMenuUI.SetActive(false);
+        isPaused = false;
+
+        if (playerInput != null)
+            playerInput.enabled = true;
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void QuitGame()
+    {
+        Time.timeScale = 1f;
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
