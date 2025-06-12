9bc8043ad4b679b27eafe45df1871c2aabf0a132
nyunn2 <jungyun_01@snu.ac.kr>
2025-06-12 09:55:21 +0900

    feat: game flow 관련 테스트 코드 추가

--- Assets/Scripts/Stage/StageClearCondition.cs
+++ Assets/Scripts/Stage/StageClearCondition.cs
@@ -26,4 +26,10 @@ public class StageClearCondition : MonoBehaviour
     }
 
     public bool TimeOver => elapsed > TimeLimit;
+
+    // 테스트용 수동 타임아웃 트리거
+    public void TriggerTimeout()
+    {
+        elapsed = TimeLimit + 1f;
+    }
 }
