5925f36dfebc86b0117f0aeb1d5176dc96be1812
nyunn2 <jungyun_01@snu.ac.kr>
2025-06-20 21:24:55 +0900

    fix: stage1 UI controller 수정

--- Assets/Scripts/Stage/StageUIController.cs
+++ Assets/Scripts/Stage/StageUIController.cs
@@ -7,7 +7,6 @@ public class StageUIController : MonoBehaviour
     [Header("UI Elements")]
     public TMP_Text timerText;
     public Image skillIcon;
-    public TMP_Text skillDescription;
     public GameObject pauseMenuUI;
     public GameObject gameOverUI;
     public GameObject gameClearUI;
@@ -15,23 +14,9 @@ public class StageUIController : MonoBehaviour
 
     private ISkill currentSkill;
 
-    // 리팩토링할 때 설명도 주입 받게 수정하기
     public void SetSkill(ISkill skill)
     {
         currentSkill = skill;
-
-        if (skill is WallWalkSkill)
-        {
-            skillDescription.text = "[Skill] Press E, Walk on walls";
-        }
-        else if (skill is TimeStopSkill)
-        {
-            skillDescription.text = "[Skill] Press E, Stop time";
-        }
-        else
-        {
-            skillDescription.text = "[Skill] No skill assigned";
-        }
     }
 
     public void SetSkillAvailability(bool available)
